4c51686ff41a5957fe35397995e0ee4d0410f939
Sinoj Kokulasingam <sinoj@login1.hpc.ut.ee>
2024-03-01 12:52:56 +0200

    Added scripts

---
+++ classify_pc.py
@@ -0,0 +1,45 @@
+import numpy as np
+import laspy
+import sklearn
+import os
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+
+
+os.chdir('../analysis')
+pc_file = 'data/lln_ground.las'
+
+
+las = laspy.read(pc_file)
+print('read las file')
+
+x, y, z = las.x, las.y, las.z
+
+
+points = np.vstack((las.x, las.y, las.z)).transpose()
+colors = np.vstack((las.red, las.green, las.blue)).transpose()
+
+
+# Geometric features extraction
+def extract_geometric_features(points):
+    neigh = NearestNeighbors(n_neighbors=3)
+    neigh.fit(points)  # Fit using x, y, z coordinates
+    distances, indices = neigh.kneighbors(points)
+    avg_distance = np.mean(distances, axis=1)
+    return avg_distance
+
+# Color features extraction (assuming colors are in 16-bit)
+def extract_color_features(colors):
+    # Normalize colors to 8-bit if necessary
+    colors_normalized = colors / 65535 * 255
+    avg_color = np.mean(colors_normalized, axis=1)
+    return avg_color
+
+# Extract features
+geometric_features = extract_geometric_features(points)
+color_features = extract_color_features(colors)
+print('kmeans is done')
+# Combine features for classification input
+features = np.column_stack((geometric_features, color_features))
+
+print(features[:, 0])
