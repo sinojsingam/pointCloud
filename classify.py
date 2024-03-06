e1e8193791f16cfb75deaac79f2c0205cc6828a6
Sinoj Kokulasingam <78551577+sinojsingam@users.noreply.github.com>
2024-03-06 10:41:43 +0100

    Update classify.py

--- classify.py
+++ classify.py
@@ -38,7 +38,7 @@ features_lln = np.vstack((full['Omnivariance (0.5)'],
 
 end_read = time.time()
 print(f"Read in {(end_read-start_read)/60} mins. Now training...")
-start_train = time.time()
+
 # Random Forest classifier
 clf = RandomForestClassifier(n_estimators=300)
 clf.fit(features_train, training_labels)
@@ -52,20 +52,18 @@ print(f"Trained in {(end_train-start_train)/60} mins. Now fitting...")
 start_fit = time.time()
 gb_clf.fit(features_train, training_labels)
 end_fit = time.time()
-print(f"Fit in {(end_fit-start_fit)/60} mins. Now predicting...")
-start_pred = time.time()
-# result
+print(f"Fit in {(end_fit-end_read)/60} mins. Now predicting...")
 
+# result
 predictions_GBT = gb_clf.predict(features_lln)
 predictions_RF = clf.predict(features_lln)
 end_pred = time.time()
-print(f"Fit in {(end_pred-start_pred)/60} mins. Now writing...")
+print(f"Predicted in {(end_pred-end_fit)/60} mins. Now writing...")
 # For evaluation
 # accuracy = accuracy_score(full_dataset_labels, predictions)
 # print("Accuracy:", accuracy)
 # Assigning predictions back to full dataset points (for visualization or further analysis)
 # add predictions to LAS data
-
 full.add_extra_dims([laspy.ExtraBytesParams(name='RF', type=np.float64),
                     laspy.ExtraBytesParams(name='GBT', type=np.float64)
 ])
@@ -74,7 +72,13 @@ full['RF'] = predictions_RF
 full['GBT'] = predictions_GBT
 
 full.write("results/lln_classified.las")
-
 done_time = time.time()
 
-print(f"Writing took {(end_pred - done_time)/60} mins. Classification without color data is done.\nThe whole process elapsed {(done_time - start_read)/3600} hours.\nClassified LAS file saved in ./results/lln_classified.las.\nGoodbye.")
+print(f"Writing took {(done_time-end_pred)/60} mins. Classification without color data is done.\nThe whole process elapsed {(done_time - start_read)/3600} hours.\nClassified LAS file saved in ./results/lln_classified.las.\nGoodbye.")
+
+#add mailme to CLI and get an email notification sent when scipt is done
+if len(sys.argv) >1:
+    if sys.argv[1]=='mailme':
+        send_email.sendNotification(f'Process finished. Classification without color data is done.\nThe whole process elapsed {(done_time - start_read)/3600} hours')
+
+
